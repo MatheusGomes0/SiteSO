2828405acf4d10e0e29aa4a6f0b296c4d6325e57
Matheus Gomes <115513930+MatheusGomes0@users.noreply.github.com>
2023-08-23 19:48:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Função para fazer a navegação suave entre as seções
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
